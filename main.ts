be41e942918155ba9eb9da5879d6efeb2c03a41b
Luis-Profe <98078028+Luis-Profe@users.noreply.github.com>
2022-05-01 20:04:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.showNumber(randint(0, 100))
+input.onPinPressed(TouchPin.P1, function () {
+	
 })
 basic.showString("Love meter")
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+    if (input.pinIsPressed(TouchPin.P1)) {
+        basic.showNumber(randint(0, 100))
+    }
 })
